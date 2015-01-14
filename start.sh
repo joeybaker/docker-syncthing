dcdaab8f97209f0beb0e0c5508fa9c1e314dfd12
Joey Baker <joey@byjoeybaker.com>
2015-01-14 00:49:05 -0500

    fix bash syntax #oops

--- start.sh
+++ start.sh
@@ -5,9 +5,10 @@ IFS=$'\n\t'
 
 # if this if the first run, generate a useful config
 if [ ! -f /srv/config/config.xml ]; then
+  echo "generating config"
   /srv/syncthing/syncthing --generate="/srv/config"
   # don't take the whole volume with the default so that we can add additional folders
-	sed -e "s/id=\"default\" path=\"/root/Sync/\"/id=\"default\" path=\"/srv/data/default\"/" -i /srv/config/config.xml
+	sed -e "s/id=\"default\" path=\"/root\/Sync\"/id=\"default\" path=\"/srv/data/default\"/" -i /srv/config/config.xml
   # ensure we can see the web ui outside of the docker container
 	sed -e "s/<address>127.0.0.1:8080/<address>0.0.0.0:8080/" -i /srv/config/config.xml
 fi
