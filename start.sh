6f07a79e03263b9b7a226ee1f743569f7e2bfb6f
Joey Baker <joey@byjoeybaker.com>
2015-09-09 10:56:07 -0700

    fix #3: escaping now works #oops

--- start.sh
+++ start.sh
@@ -8,13 +8,13 @@ if [ ! -f /srv/config/config.xml ]; then
   echo "generating config"
   /srv/syncthing/syncthing --generate="/srv/config"
   # don't take the whole volume with the default so that we can add additional folders
-	sed -e "s/id=\"default\" path=\"/root\/Sync\"/id=\"default\" path=\"/srv/data/default\"/" -i /srv/config/config.xml
+  sed -e "s/id=\"default\" path=\"\/root\/Sync\"/id=\"default\" path=\"\/srv\/data\/default\"/" -i /srv/config/config.xml
   # ensure we can see the web ui outside of the docker container
 	sed -e "s/<address>127.0.0.1:8384/<address>0.0.0.0:8080/" -i /srv/config/config.xml
 fi
 
 # set permissions so that we have access to volumes
-chown -R syncthing:users /srv/config /srv/data 
+chown -R syncthing:users /srv/config /srv/data
 chmod -R 770 /srv/config /srv/data
 
 gosu syncthing /srv/syncthing/syncthing -home=/srv/config
